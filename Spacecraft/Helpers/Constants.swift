68562e9e26698c2becfcecd070729369a462b04c
Cassio Diego T. Campos <cassiodiego@icloud.com>
2020-10-01 01:12:42 -0300

    swiftlint

--- Spacecraft/Helpers/Constants.swift
+++ Spacecraft/Helpers/Constants.swift
@@ -9,49 +9,51 @@
 import Foundation
 
 struct Constants {
-    
+
     struct Assets {
-        static let firstBackground:String = "background"
-        static let rockOne:NSString = "rock_1"
-        static let rockTwo:NSString = "rock_2"
-        static let rockThree:NSString = "rock_3"
-        static let whiteStar:String = "star"
-        static let yellowStar:String = "yellowStar"
-        static let orangeShot:String = "shot"
-        static let blueShot:String = "shot2"
-        static let Armory:String = "Armory"
-        static let Rinzler:String = "Rinzler"
+        static let firstBackground: String = "background"
+        static let rockOne: NSString = "rock_1"
+        static let rockTwo: NSString = "rock_2"
+        static let rockThree: NSString = "rock_3"
+        static let whiteStar: String = "star"
+        static let yellowStar: String = "yellowStar"
+        static let orangeShot: String = "shot"
+        static let blueShot: String = "shot2"
+        static let Armory: String = "Armory"
+        static let Rinzler: String = "Rinzler"
     }
-    
+
     struct Directions {
-        static let left:String = "left"
-        static let right:String = "right"
+        static let left: String = "left"
+        static let right: String = "right"
     }
-    
+
     struct CollisionCategories {
-        static let rockCategory:UInt32 = 0x1 << 1
-        static let shotCategory:UInt32 = 0x1 << 0
-        static let playerCategory:UInt32 = 0x1 << 0
+        static let rockCategory: UInt32 = 0x1 << 1
+        static let shotCategory: UInt32 = 0x1 << 0
+        static let playerCategory: UInt32 = 0x1 << 0
         static let EdgeBody: UInt32 = 0x1 << 4
     }
-    
+
     struct DataConfigKeys {
-        static let highscore:String = "highscore"
-        static let ship:String = "ship"
-        static let soundStatus:String = "soundStatus"
-        static let musicStatus:String = "musicStatus"
+        static let highscore: String = "highscore"
+        static let ship: String = "ship"
+        static let soundStatus: String = "soundStatus"
+        static let musicStatus: String = "musicStatus"
     }
     struct GameCenterConfig {
-        static let leaderboardId:String = "LeaderboardSpacecraftI"
+        static let leaderboardId: String = "LeaderboardSpacecraftI"
     }
-    
+
     struct GameConfigInitialValues {
         static let velocity = 200/1
     }
-    
+    struct SupportedLanguages {
+        static let english: [String] = ["en"]
+        static let portuguese: [String] = ["pt", "pt-BR", "pt-PT"]
+        static let spanish: [String] = ["es", "es-ES", "es-HN", "es-CO", "es-PA", "es-SV", "es-CR", "es-PE", "es-BO", "es-GQ", "es-MX", "es-GT", "es-419", "es-AR", "es-PR", "es-US", "es-NI", "es-PY", "es-UY", "es-DO", "es-CL", "es-VE"]
+    }
     struct Levels {
-        struct LevelOne {
-        }
     }
 
 }
--- Spacecraft/Scene/GameOverScene.swift
+++ Spacecraft/Scene/GameOverScene.swift
@@ -12,127 +12,78 @@ import UIKit
 import GameKit
 
 class GameOverScene: GameSceneObjects {
-    
-    var Background:SKSpriteNode = SKSpriteNode()
+
+    var background: SKSpriteNode = SKSpriteNode()
 
     var viewController: UIViewController?
-    
-    init(size:CGSize, won:Bool, score:String){
-        
+    let spanish = Constants.SupportedLanguages.spanish.self
+    let portuguese = Constants.SupportedLanguages.portuguese.self
+    let english = Constants.SupportedLanguages.english.self
+
+    init(size: CGSize, won: Bool, score: String) {
+
         super.init(size: size)
-        
+
         let preferredLanguage = NSLocale.preferredLanguages[0] as String
 
         self.backgroundColor = SKColor.black
-        
-        var message:NSString = NSString()
-        var scoreLabel:NSString = NSString()
-        
+
+        var message: NSString = NSString()
+        var scoreLabel: NSString = NSString()
+
         let screenSize = UIScreen.main.bounds
         _ = screenSize.width
         _ = screenSize.height
-        
-        Background = SKSpriteNode(imageNamed: "background")
-        Background.position = CGPoint(x: (screenSize.width * 0.40), y: (screenSize.height * 0.500))
 
-        if (won){
-            
-            if preferredLanguage == "pt-BR" {
-                
+        background = SKSpriteNode(imageNamed: "background")
+        background.position = CGPoint(x: (screenSize.width * 0.40), y: (screenSize.height * 0.500))
+
+        if (won) {
+            if portuguese.contains(preferredLanguage) {
                 message = "VOCÊ GANHOU, PARABÉNS!"
                 scoreLabel = "SCORE: \(score)" as NSString
-                
-            } else if preferredLanguage == "es" || preferredLanguage == "es-ES" || preferredLanguage == "es-HN" || preferredLanguage == "es-CO" || preferredLanguage == "es-PA" || preferredLanguage == "es-SV" || preferredLanguage == "es-CR" || preferredLanguage == "es-PE" || preferredLanguage == "es-BO" || preferredLanguage == "es-GQ" || preferredLanguage == "es-MX" || preferredLanguage == "es-GT" || preferredLanguage == "es-419" || preferredLanguage == "es-AR" || preferredLanguage == "es-PR" || preferredLanguage == "es-US" || preferredLanguage == "es-NI" || preferredLanguage == "es-PY" || preferredLanguage == "es-UY" || preferredLanguage == "es-DO" || preferredLanguage == "es-CL" || preferredLanguage == "es-VE" {
-                
+            } else if spanish.contains(preferredLanguage) {
                 message = "TE HAS GANADO, ¡PARABÉNS!"
                 scoreLabel = "SCORE: \(score)" as NSString
-                
             } else {
-                
                 message = "YOU WIN, CONGRATULATIONS!"
                 scoreLabel = "SCORE: \(score)" as NSString
             }
-            
-        } else {
-            
-            if preferredLanguage == "pt-BR" {
 
+        } else {
+            if portuguese.contains(preferredLanguage) {
                 message = "VOCÊ PERDEU"
                 scoreLabel = "PONTUAÇÃO: \(score)" as NSString
-            
-            } else if preferredLanguage == "es" || preferredLanguage == "es-ES" || preferredLanguage == "es-HN" || preferredLanguage == "es-CO" || preferredLanguage == "es-PA" || preferredLanguage == "es-SV" || preferredLanguage == "es-CR" || preferredLanguage == "es-PE" || preferredLanguage == "es-BO" || preferredLanguage == "es-GQ" || preferredLanguage == "es-MX" || preferredLanguage == "es-GT" || preferredLanguage == "es-419" || preferredLanguage == "es-AR" || preferredLanguage == "es-PR" || preferredLanguage == "es-US" || preferredLanguage == "es-NI" || preferredLanguage == "es-PY" || preferredLanguage == "es-UY" || preferredLanguage == "es-DO" || preferredLanguage == "es-CL" || preferredLanguage == "es-VE" {
-            
+            } else if spanish.contains(preferredLanguage) {
                 message = "JUEGO TERMINADO"
                 scoreLabel = "PUNTUACIÓN: \(score)" as NSString
-                
             } else {
-                
                 message = "GAME OVER"
                 scoreLabel = "SCORE: \(score)" as NSString
-                
             }
-                
+
         }
-        
-        var highscore:String
-        
-        let test = highscoreAlreadyExist(key: "highscore")
-        
-        if test {
-            
-            highscore = UserDefaults.standard.object(forKey: "highscore")! as! String
-            
-        } else {
-            
-            highscore = "0"
-            
-        }
-        
-        if (Int(score))! > (Int(highscore))!{
-            
+        var highscore: String
+        Utils.alreadyExistDataForKey(key: Constants.DataConfigKeys.highscore) ? (highscore = (UserDefaults.standard.object(forKey: "highscore")! as? String)!) : (highscore = "0")
+
+        if (Int(score))! > (Int(highscore))! {
             UserDefaults.standard.set(score, forKey: "highscore")
-            
-            //TO-DO: GameCenterController().sync()
-            let leaderboardID = "LeaderboardSpacecraftI"
-            let sScore = GKScore(leaderboardIdentifier: leaderboardID)
-            sScore.value = Int64(score)!
-            
-            GKScore.report([sScore], withCompletionHandler: { (error: Error?) -> Void in
-                if error != nil {
-                    //print(error!.localizedDescription)
-                } else {
-                    //print("Score submitted")
-                    
-                }
-            })
-            
-        } else {
-            
-            if test {
-                
-                highscore = UserDefaults.standard.object(forKey: "highscore")! as! String
-                
-            } else {
-                
-                highscore = "0"
-                
-            }            
+            GameCenterViewController().syncScore()
         }
-        
-        self.addChild(Background)
-        
-        let bgScore:SKSpriteNode = SKSpriteNode(imageNamed: "score2")
+        self.addChild(background)
+
+        let bgScore: SKSpriteNode = SKSpriteNode(imageNamed: "score2")
         bgScore.position = CGPoint(x: (screenSize.width * 0.50), y: (screenSize.height * 0.834))
         bgScore.setScale(0.76)
         bgScore.zPosition = 3
         self.addChild(bgScore)
-        
-        let logo:SKSpriteNode = SKSpriteNode(imageNamed: "SpacecraftLogo")
+
+        let logo: SKSpriteNode = SKSpriteNode(imageNamed: "SpacecraftLogo")
         logo.position = CGPoint(x: (screenSize.width * 0.50), y: (screenSize.height * 0.690))
         logo.zPosition = 3
         logo.setScale(0.22)
         self.addChild(logo)
-        
+
         let label1 = SKLabelNode(fontNamed: "Helvetica")
         label1.text = message as String
         label1.fontColor = SKColor.white
@@ -140,7 +91,7 @@ class GameOverScene: GameSceneObjects {
         label1.position = CGPoint(x: (screenSize.width * 0.50), y: (screenSize.height * 0.540))
         label1.zPosition = 3
         self.addChild(label1)
-        
+
         let label2 = SKLabelNode(fontNamed: "Helvetica")
         label2.text = scoreLabel as String
         label2.fontSize = 20
@@ -148,36 +99,31 @@ class GameOverScene: GameSceneObjects {
         label2.position = CGPoint(x: (screenSize.width * 0.50), y: (screenSize.height * 0.465))
         label2.zPosition = 4
         self.addChild(label2)
-        
+
         let label3 = SKLabelNode(fontNamed: "Helvetica")
-        if Int(score)! < Int(highscore)!{ label3.text = "\(highscore)" }
-        else { label3.text = "\(score)" }
+        if Int(score)! < Int(highscore)! {
+            label3.text = "\(highscore)"
+        } else { label3.text = "\(score)" }
         label3.fontSize = 35
         label3.fontColor = SKColor.white
         label3.position = CGPoint(x: (screenSize.width * 0.50), y: (screenSize.height * 0.830))
         label3.zPosition = 2
         self.addChild(label3)
-        
         let label4 = SKLabelNode(fontNamed: "Helvetica")
         if preferredLanguage == "pt-BR" {
-            
             label4.text = "TOQUE NA TELA PARA RECOMEÇAR"
-            
-            } else if preferredLanguage == "es" || preferredLanguage == "es-ES" || preferredLanguage == "es-HN" || preferredLanguage == "es-CO" || preferredLanguage == "es-PA" || preferredLanguage == "es-SV" || preferredLanguage == "es-CR" || preferredLanguage == "es-PE" || preferredLanguage == "es-BO" || preferredLanguage == "es-GQ" || preferredLanguage == "es-MX" || preferredLanguage == "es-GT" || preferredLanguage == "es-419" || preferredLanguage == "es-AR" || preferredLanguage == "es-PR" || preferredLanguage == "es-US" || preferredLanguage == "es-NI" || preferredLanguage == "es-PY" || preferredLanguage == "es-UY" || preferredLanguage == "es-DO" || preferredLanguage == "es-CL" || preferredLanguage == "es-VE" {
-            
+            } else if spanish.contains(preferredLanguage) {
+
             label4.text = "TOQUE PARA REINICIAR"
-            
         } else {
-            
             label4.text = "TOUCH TO RESTART"
-            
         }
         label4.fontSize = 10
         label4.fontColor = SKColor.white
         label4.position = CGPoint(x: (screenSize.width * 0.50), y: (screenSize.height * 0.360))
         label4.zPosition = 1
         self.addChild(label4)
-        
+
         let label5 = SKLabelNode(fontNamed: "Helvetica")
         label5.text = "spacecraft.com.br"
         label5.fontSize = 18
@@ -185,51 +131,32 @@ class GameOverScene: GameSceneObjects {
         label5.position = CGPoint(x: (screenSize.width * 0.50), y: (screenSize.height * 0.100))
         label5.zPosition = 1
         self.addChild(label5)
-        
+
         let label6 = SKLabelNode(fontNamed: "Helvetica")
-        
         if preferredLanguage == "pt-BR" {
-            
             label6.text = "contato@cassiodiego.com"
-            
-            } else if preferredLanguage == "es" || preferredLanguage == "es-ES" || preferredLanguage == "es-HN" || preferredLanguage == "es-CO" || preferredLanguage == "es-PA" || preferredLanguage == "es-SV" || preferredLanguage == "es-CR" || preferredLanguage == "es-PE" || preferredLanguage == "es-BO" || preferredLanguage == "es-GQ" || preferredLanguage == "es-MX" || preferredLanguage == "es-GT" || preferredLanguage == "es-419" || preferredLanguage == "es-AR" || preferredLanguage == "es-PR" || preferredLanguage == "es-US" || preferredLanguage == "es-NI" || preferredLanguage == "es-PY" || preferredLanguage == "es-UY" || preferredLanguage == "es-DO" || preferredLanguage == "es-CL" || preferredLanguage == "es-VE" {
-            
+            } else if spanish.contains(preferredLanguage) {
             label6.text = "contacto@cassiodiego.com"
-            
         } else {
-            
             label6.text = "contact@cassiodiego.com"
-            
         }
-        
         label6.fontSize = 14
         label6.fontColor = SKColor.yellow
         label6.position = CGPoint(x: (screenSize.width * 0.50), y: (screenSize.height * 0.070))
         label6.zPosition = 1
         self.addChild(label6)
-        
     }
-    
     required init?(coder aDecoder: NSCoder) {
-        
         fatalError("init(coder:) has not been implemented")
-        
     }
-    
     override func didMove(to view: SKView) { }
-    
-    func highscoreAlreadyExist(key: String) -> Bool {
-        
-        return UserDefaults.standard.object(forKey: key) != nil}
-    
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         let touch = touches.first
         let touchLocation = touch!.location(in: self)
         if touchLocation.x != 0 {
-            let transition:SKTransition = SKTransition.flipHorizontal(withDuration: 0.5)
-            let gameScene:SKScene = GameScene(size: self.size)
+            let transition: SKTransition = SKTransition.flipHorizontal(withDuration: 0.5)
+            let gameScene: SKScene = GameScene(size: self.size)
             self.view!.presentScene(gameScene, transition: transition)
         }
     }
-    
 }
